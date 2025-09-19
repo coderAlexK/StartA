c986787b561dc4b18fcce233b837fc4a887dbc75
AlexK <a.privet.narnia@gmail.com>
2025-09-19 17:37:43 +0300

    init 1

--- app/src/main/java/com/akozlov/starta/ui/theme/Theme.kt
+++ app/src/main/java/com/akozlov/starta/ui/theme/Theme.kt
@@ -1,6 +1,6 @@
 package com.akozlov.starta.ui.theme
 
-import android.app.Activity
+
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
