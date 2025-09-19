6d51ec80395d983853e9e61d1eb5dad17200823c
AlexK <a.privet.narnia@gmail.com>
2025-09-19 17:37:43 +0300

    init 1

--- app/src/main/java/com/akozlov/starta/HomeScreen.kt
+++ app/src/main/java/com/akozlov/starta/HomeScreen.kt
@@ -3,7 +3,6 @@ package com.akozlov.starta
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
